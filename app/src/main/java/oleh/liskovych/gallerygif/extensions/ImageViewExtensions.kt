8807193ed2a7e2cae563091019642d18a0d46b70
Oleg  Liskovich <oleg.l@powercode.us>
2019-01-15 10:44:45 +0200

    Gallery. Work in progress

--- app/src/main/java/oleh/liskovych/gallerygif/extensions/ImageViewExtensions.kt
+++ app/src/main/java/oleh/liskovych/gallerygif/extensions/ImageViewExtensions.kt
@@ -6,6 +6,14 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.RequestOptions
 
+fun ImageView.loadImage(imageUri: String, @DrawableRes placeholder: Int) {
+    Glide.with(this.context)
+        .load(imageUri)
+        .apply(RequestOptions.diskCacheStrategyOf(DiskCacheStrategy.ALL))
+        .apply(RequestOptions().centerCrop().placeholder(placeholder).error(placeholder))
+        .into(this)
+}
+
 fun ImageView.loadCircularImage(imageUri: String, @DrawableRes placeholder: Int, @DrawableRes error: Int) {
     Glide.with(this.context)
         .load(imageUri)
@@ -17,3 +25,4 @@ fun ImageView.loadCircularImage(imageUri: String, @DrawableRes placeholder: Int,
                 .circleCrop())
         .into(this)
 }
+
--- app/src/main/java/oleh/liskovych/gallerygif/ui/screens/main/gallery/GalleryFragment.kt
+++ app/src/main/java/oleh/liskovych/gallerygif/ui/screens/main/gallery/GalleryFragment.kt
@@ -1,17 +1,34 @@
 package oleh.liskovych.gallerygif.ui.screens.main.gallery
 
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.fragment_gallery.*
 import oleh.liskovych.gallerygif.R
 import oleh.liskovych.gallerygif.ui.base.BaseFragment
+import org.jetbrains.anko.support.v4.ctx
 
-class GalleryFragment: BaseFragment<GalleryViewModel>() {
+class GalleryFragment: BaseFragment<GalleryViewModel>(), GalleryAdapterCallback {
 
     override val viewModelClass = GalleryViewModel::class.java
 
     override val layoutId = R.layout.fragment_gallery
 
+    private val adapter by lazy { GalleryAdapter(ctx, this) }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView() {
+        rvGrid.adapter = adapter
+    }
+
     override fun observeLiveData(viewModel: GalleryViewModel) {
         // do nothing
     }
 
-
+    override fun onItemClickListener() {
+        // do nothing
+    }
 }
